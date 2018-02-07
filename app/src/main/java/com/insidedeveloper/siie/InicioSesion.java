3a46f463d94b6be6e34e742d4d0ece047183b407
Jorge Castillo <serpifox@gmail.com>
2018-02-07 17:27:52 -0600

    Initial commit

---
+++ app/src/main/java/com/insidedeveloper/siie/InicioSesion.java
@@ -0,0 +1,13 @@
+package com.insidedeveloper.siie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
