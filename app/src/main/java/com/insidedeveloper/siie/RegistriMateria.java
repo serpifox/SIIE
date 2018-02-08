d13f544284880cac3ec6f94a60cb94f96c1110cc
Jorge Castillo <serpifox@gmail.com>
2018-02-07 20:07:18 -0600

    Registro de materia

---
+++ app/src/main/java/com/insidedeveloper/siie/RegistriMateria.java
@@ -0,0 +1,107 @@
+package com.insidedeveloper.siie;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RegistriMateria extends AppCompatActivity {
+
+    EditText etnrc, etnombre;
+    Button btnregistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registri_materia);
+
+        etnrc = findViewById(R.id.etNRC);
+        etnombre = findViewById(R.id.etNombre);
+        btnregistro = findViewById(R.id.btnRegistrar);
+
+        btnregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Registrar_Materia().execute("http://10.0.2.2/SIIE/Registro_Materia.php?NRC="+etnrc.getText().toString()+"&Nombre="+etnombre.getText().toString());
+            }
+        });
+    }
+
+    private class Registrar_Materia extends AsyncTask <String, Void, String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            }catch (IOException e){
+                return "No se puede recuperar la página web URL puede ser válido...";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se almacenaron los datos correctamente", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    /* Dado un URL, establece un conexion HttpURLConnection y respuesta
+       El contenido de la página web lo crea un InputStream, que se vuelve
+     una cadena.*/
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Mostrar sólo los primeros 500 caracteres del
+        // contenido de la página web.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Inicia la consulta
+            conn.connect();
+            //EL metodo getResponseCode devuelve un codigo de estado,
+            // donde un codigo 200 significa exito en la conexion
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convertir el InputStream en una cadena
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Asegurarnos de que el InputStream se cierra después de que la aplicación ha
+            // terminado de utilizarlo.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    // Lee un InputStream y lo convierte en una cadena.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+}
